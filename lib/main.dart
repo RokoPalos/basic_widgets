a47bb06005ed12a1544c37ee33effbd32f708bc4
Roko <roko.palos@skole.hr>
2026-04-09 13:45:33 +0200

    meow

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
-
+const img_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrE9aJtgq4T4ewkV-lGkTjo693YXxLC94hPQ&s';
+const loremipsum =
+'''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua''';
 void main() {
-  runApp((MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Home page'),
+    runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('Home Page'),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                img_url,
+                fit: BoxFit.cover
+              ),
+              Text(
+                loremipsum,
+                style: TextStyle(fontSize: 14),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Center(
-        child: Text('AAAA'),
-      )
-    ),
-  )),
+    );
 
-  );
 }
